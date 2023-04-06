c8b8237ee1d23113394aae8f3f36a73bb842c194
kevinhng77 <62964063+kevinhng77@users.noreply.github.com>
2023-04-05 21:36:37 -0700

    pseudo code

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,24 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+var currentDayz = $("currentDay");
+currentDayz.textContent= getDate();
+console.log(Date.now())
+
 $(function () {
+  var id = $(this).attr('id')
+  $("button").click(function(){
+    localStorage.setItem(id,$('#textarea').val())
+  })
+
+  // create selector for currentday
+  //create function to gather the time using jquery, set it to the variable, display the text
+  // index the variable to only have the first hour of the variable
+  // if statement to color the boxes if its less than or more than the variable
+  // local storage linked to save button
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -21,3 +38,5 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+
